61fba8b052c9a09e9f4e9e2dc2a83d5f67f60612
Viliam <vatvil@gmail.com>
2024-05-22 20:10:43 +0200

    add ejs and 404 handling

--- app.js
+++ app.js
@@ -3,13 +3,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const errorController = require("./controllers/errorController");
+
 const indexRouter = require("./routes/index");
 const adminRouter = require("./routes/admin");
 const articlesRouter = require("./routes/articles");
 
+// This line tells Express to use EJS (Embedded JavaScript) as the templating engine.
+app.set("view engine", "ejs");
+// This line sets the directory where your EJS templates (views) are located.
+app.set("views", "views");
+
 app.use(indexRouter);
 app.use("/admin", adminRouter);
 app.use("/articles", articlesRouter);
+
+app.use(errorController.get404);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
